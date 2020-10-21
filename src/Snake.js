754e7f774b09302437bb715f85b1e8d56f64ee08
Ben Wood <Ben.Wood@office.borwell.com>
2020-10-21 14:36:27 +0100

    Added food and snake

--- src/Snake.js
+++ src/Snake.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Snake.css";
 
-function Snake() {
-  const grid = Array(100).fill("");
+const Snake = () => {
+  const [alive, setAlive] = useState(true);
+  const grid = initializeGrid();
+
+  function initializeGrid() {
+    const newGrid = Array(100).fill("Empty");
+    newGrid[55] = "Food";
+    newGrid[25] = "Snake";
+    return newGrid;
+  }
+
+  function newFood() {
+    grid[Math.floor(Math.random() * 100)] = "Food";
+  }
 
   return (
-    <div className="Grid">
-      {grid.map((square, index) => (
-        <div key={index} className="Square" />
-      ))}
+    <div className="Game">
+      <h1>Snake</h1>
+      <>
+        {alive ? (
+          <div className="Grid">
+            {grid.map((square, index) => (
+              <div key={index} className={square} />
+            ))}
+          </div>
+        ) : (
+          <h2>Game Over!</h2>
+        )}
+      </>
     </div>
   );
-}
+};
 
 export default Snake;
