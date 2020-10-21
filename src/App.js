7ce88cdd58289f71a1fdc113074861bb5388d99e
Ben Wood <Ben.Wood@office.borwell.com>
2020-10-21 13:17:21 +0100

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import "./App.css";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <i alt="games" className="material-icons Games">
-          games
-        </i>
-        <p>Welcome to JavaScriptGames</p>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
