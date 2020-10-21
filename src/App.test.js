ec69d5b4669e350dfdc1c0f37f2febf95ab19347
Ben Wood <Ben.Wood@office.borwell.com>
2020-10-21 12:22:20 +0100

    Changed homepage from default

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders games icon", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const icon = getByText("games");
+  expect(icon).toBeInTheDocument();
+});
+
+test("renders header text", () => {
+  const { getByText } = render(<App />);
+  const text = getByText("Welcome to JavaScriptGames");
+  expect(text).toBeInTheDocument();
 });
